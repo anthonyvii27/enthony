f993affb66c14fb2a5addc0b25691ade131440ef
Anthony <anthony.vii27@gmail.com>
2021-07-08 21:52:38 -0300

    feat: added external links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
 export default function Home() {
   return (
     <div>
-      <h1>Olá pessoal!</h1>
+      <h1>Site em desenvolvimento - Obrigado</h1>
+      <h2>Links</h2>
+
+      <a href="https://www.instagram.com/vii.enthony/" target="_blank" rel="noreferrer noopener">
+        Instagram
+      </a>
+
+      <a href="https://www.tiktok.com/@viienthony?lang=en" target="_blank" rel="noreferrer noopener">
+        Tiktok
+      </a>
+
+      <a href="https://www.youtube.com/channel/UCNPvNQl3i3LOB2pFSvJ2XKQ" target="_blank" rel="noreferrer noopener">
+        Youtube
+      </a>
+
+      <a href="https://twitter.com/viienthony" target="_blank" rel="noreferrer noopener">
+        Twitter
+      </a>
+
+      <a href="https://t.me/joinchat/b_H5fZu4zQwyOGRh" target="_blank" rel="noreferrer noopener">
+        Telegram
+      </a>
     </div>
   );
 }
