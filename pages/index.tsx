741dad3de36f2b4f6eb94049069134f284465230
Anthony <anthony.vii27@gmail.com>
2021-07-08 21:56:39 -0300

    feat: added a simple css on external links page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,28 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Site em desenvolvimento - Obrigado</h1>
-      <h2>Links</h2>
+    <div className="container">
+      <h1>Site em desenvolvimento - Obrigado</h1><br />
+      <h2>Links</h2><br />
 
       <a href="https://www.instagram.com/vii.enthony/" target="_blank" rel="noreferrer noopener">
         Instagram
-      </a>
+      </a><br />
 
       <a href="https://www.tiktok.com/@viienthony?lang=en" target="_blank" rel="noreferrer noopener">
         Tiktok
-      </a>
+      </a><br />
 
       <a href="https://www.youtube.com/channel/UCNPvNQl3i3LOB2pFSvJ2XKQ" target="_blank" rel="noreferrer noopener">
         Youtube
-      </a>
+      </a><br />
 
       <a href="https://twitter.com/viienthony" target="_blank" rel="noreferrer noopener">
         Twitter
-      </a>
+      </a><br />
 
       <a href="https://t.me/joinchat/b_H5fZu4zQwyOGRh" target="_blank" rel="noreferrer noopener">
         Telegram
-      </a>
+      </a><br />
     </div>
   );
 }
